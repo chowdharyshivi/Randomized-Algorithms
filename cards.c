0bbf0555c050e8c52fcd5d59f87af1f74c4008a4
Rishika <maheshwaririshika810@gmail.com>
2020-10-07 19:39:13 +0530

    Card Code Modified

--- cards.c
+++ cards.c
@@ -2,21 +2,16 @@
 #include<time.h>
 #include<stdlib.h>
 #include<string.h>
- 
+ void compute(int limit,char name[4][9],char number[13][6]);
 typedef struct{
     char *value;
     char *type;
 }card;
- 
-int main(){
-	
-	int i,r,j=0,limit;
-	int count=0;
+
+void compute(int limit,char name[4][9],char number[13][6]){
+    int i,j=0,count=0;
+    
 	float answer;
-    printf("enter the iterations: \n");
-    scanf("%d",&limit);
-	char name[4][9]={{"diamonds"},{"clubs"},{"hearts"},{"spades"}};
-	char number[13][6]={{"ace"},{"two"},{"three"},{"four"},{"five"},{"six"},{"seven"},{"eight"},{"nine"},{"ten"},{"Jack"},{"Queen"},{"King"}};
     card c[52];
     card temp;
 for(i=0;i<52;i++){
@@ -25,17 +20,17 @@ for(i=0;i<52;i++){
 }
  
 	srand(time(NULL));
-	while(j<=limit){
+	while(j<limit){
 	    j++;
 for(i=0;i<52;i++){
-	r=(rand() % 52);
-	temp= c[i];
+int r=(rand() % 52);
+    temp= c[i];
 	c[i]=c[r];
 	c[r]=temp;
-}
-for(i=0;i<52;i++){
+
 	printf("%s %s\n",c[i].type,c[i].value);
 }
+
 for(i=0;i<52;i++){
     if((c[i].value==number[12])&&(c[i+1].value==number[12])){
         count=count+1;
@@ -47,6 +42,16 @@ printf("\n\n\n\n");
 }
 answer=(float)count/limit;
 printf("the probability is: %f",answer);
+}
+ 
+int main(){
+	
+	int limit;
+
+    printf("enter the iterations: \n");
+    scanf("%d",&limit);
+	char name[4][9]={{"diamonds"},{"clubs"},{"hearts"},{"spades"}};
+	char number[13][6]={{"ace"},{"two"},{"three"},{"four"},{"five"},{"six"},{"seven"},{"eight"},{"nine"},{"ten"},{"Jack"},{"Queen"},{"King"}};
+   compute(limit,name,number);
 return 0;
 }
-
