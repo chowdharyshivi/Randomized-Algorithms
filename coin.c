9f25e2d381f1430248eff7543a596724141b474d
Rishika <maheshwaririshika810@gmail.com>
2020-10-07 17:03:51 +0530

    Coin code modified

--- coin.c
+++ coin.c
@@ -1,40 +1,41 @@
 #include<stdio.h>
-#include<conio.h>
 #include<stdlib.h>
 #include<time.h>
-
-int main(){
-	int j,count=0,limit,i=0,head,tail;
- float y,answer,h=2.5;
- printf("enter the limit amount");
- scanf("%d",&limit);
- 
-    
-    srand(time(NULL));
-   while(i<=limit){
-   
-   i++;
-   head=0;
-   tail=0;
-for(j=0;j<10;j++){
-
- y=(float)((rand()/(float)RAND_MAX)*5);
-    printf("%f\n",y);
-    if(y<h){
-    	head=head+1;
-	}
-	else
-	{
+#include<conio.h>
+void compute(int limit,int d){
+    int count=0;
+    float h=2.5;
+    	srand(time(NULL));
+int	i=0,j;
+	while(i<limit){
+	i++;
+int head=0;
+int tail=0;
+	for(j=0;j<10;j++){
+	float y=(float)((rand()/(float)RAND_MAX)*5);
+		printf("%f\n",y);
+		if(y<h)
+		    head=head+1;
+		else
 		tail=tail+1;
 	}
-}
-if((head==3) || (head==6)||(head==9)){
-	count=count+1;
-}
-}
-printf("count: %d\n",count);
-answer=(float)count/limit;
-printf("the limit is %d \n and the probability is %f",limit,answer);
+	if((head==3)||(head==6)||(head==9)){
+	     count=count+1;
+	}
+	}
+float answer=(float)count/limit;
+printf("\nWhen a coin is tossed 10 times the probability of getting head 3,6 or 9 times is %f ",answer);
 
+    
 }
 
+int main(){
+	int limit,d;
+	
+	printf("How many times you want to iterate? ");
+	scanf("%d",&limit);
+	printf("\nHow many times you want to toss a coin?  ");
+	scanf("%d",&d);
+	compute(limit,d);
+	
+}
